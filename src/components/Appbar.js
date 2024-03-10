12e79eab0b0babc3a2ffd9eeaf348b76036ab938
Matthew Quijano <mq022002@gmail.com>
2024-03-10 08:25:27 -0400

    Revert "Testing GitHub Workflow with Easy-to-Spot Visual Change"

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -9,7 +9,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function Appbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ backgroundColor: 'blue' }}> 
+      <AppBar position="static" style={{ backgroundColor: 'red' }}> 
         <Toolbar>
           <IconButton
             size="large"
