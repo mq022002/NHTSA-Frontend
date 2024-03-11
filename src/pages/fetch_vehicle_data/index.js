a04732eab9c1651f27077aa24b3fd89d7d030bb6
hooghir <hooghirm29@gmail.com>
2024-03-11 18:43:43 -0400

    refactor: Added white background to the website

--- src/pages/fetch_vehicle_data/index.js
+++ src/pages/fetch_vehicle_data/index.js
@@ -4,7 +4,7 @@ import Cars from '../../components/Cars';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-white h-screen">
       <Appbar />
       <Cars />
       
