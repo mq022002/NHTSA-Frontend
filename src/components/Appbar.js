b156598244215474b1118c65f124caa1b87d4045
ABaerga2002 <a.baerga2002@gmail.com>
2024-03-11 00:02:48 -0400

    refactor: Navbar CSS using Tailwind

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,32 +1,15 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
 
 export default function Appbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ backgroundColor: 'red' }}> 
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          
-          <img src="/TheHartfordIcon.png" alt="logo" style={{ marginRight: '20px', height: '50px' }} />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            The Hartford
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <nav className="flex items-center flex-wrap bg-[#dbd2c4] p-6  justify-between">
+      <img src="/TheHartfordIcon.png" alt="logo" className="px-2 h-12 w-auto" />
+      {/* Put a div tag here, and inside of the div tag, put a tags */}
+      <div>
+        <a href = "/">
+          The Home Page
+        </a>
+      </div>
+    </nav>
   );
 }
