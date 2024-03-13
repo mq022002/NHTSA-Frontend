449b080ea099a995f60fe93d8c512bffe2c72e08
Matthew Quijano <mq022002@gmail.com>
2024-03-13 19:53:01 -0400

    refactor: Move Navigation into ui, for ui-related components
    - Forgot to adjust the actual component name for FetchVehicleData from App.

--- src/pages/fetch_vehicle_data/index.js
+++ src/pages/fetch_vehicle_data/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Navigation from '../../components/layout/Navigation';
+import Navigation from '../../components/ui/Navigation';
 import CarFetcher from '../../components/cars/CarFetcher';
 
-function App() {
+function FetchVehicleData() {
   return (
     <div className="bg-white min-h-screen">
       <Navigation />
@@ -12,4 +12,4 @@ function App() {
   );
 }
 
-export default App;
+export default FetchVehicleData;
