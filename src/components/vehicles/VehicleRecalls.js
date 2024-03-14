be2f02fa4c5f5c901fec62e716bfb89b94088b6a
Matthew Quijano <mq022002@gmail.com>
2024-03-14 06:06:23 -0400

    refactor: Removed MUI component in favor of more native React components

--- src/components/vehicles/VehicleRecalls.js
+++ src/components/vehicles/VehicleRecalls.js
@@ -1,4 +1,4 @@
-import { Tabs, Tab } from "@mui/material";
+import React, { useState, useEffect } from "react";
 
 const RecallInfoBox = ({ recallItem }) => (
   <div className="border border-gray-300 rounded p-4 my-2">
@@ -9,35 +9,47 @@ const RecallInfoBox = ({ recallItem }) => (
   </div>
 );
 
-const VehicleRecalls = ({ recalls, activeRecallTab, handleRecallTabChange }) => (
-  <div className="w-1/2 pl-2">
-    {recalls.length > 0 ? (
-      <>
-        <Tabs
-          value={activeRecallTab}
-          onChange={handleRecallTabChange}
-          orientation="vertical"
-          variant="scrollable"
-          className="overflow-auto"
-        >
-          {recalls.map((recall, index) => (
-            <Tab
-              key={index}
-              label={<p className="text-xs">{recall.Component}</p>}
-            />
-          ))}
-        </Tabs>
-        <div className="pt-3">
-          <h2 className="text-lg font-semibold">
-            Recall Details for {recalls[activeRecallTab].Component}
-          </h2>
-          <RecallInfoBox recallItem={recalls[activeRecallTab]} />
-        </div>
-      </>
-    ) : (
-      <p>No recall information available.</p>
-    )}
-  </div>
-);
+const VehicleRecalls = ({ recalls, handleRecallTabChange }) => {
+  const [activeRecallTab, setActiveRecallTab] = useState(0);
+
+  useEffect(() => {
+    setActiveRecallTab(0);
+  }, [recalls]);
+
+  return (
+    <div className="w-1/2 pl-2">
+      {recalls.length > 0 ? (
+        <>
+          <div className="overflow-auto flex flex-col">
+            {recalls.map((recall, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  handleRecallTabChange(index);
+                  setActiveRecallTab(index);
+                }}
+                className={`py-2 rounded text-xs my-2 ${
+                  activeRecallTab === index ? "bg-[#cc6066]" : ""
+                }`}
+              >
+                {recall.Component}
+              </button>
+            ))}
+          </div>
+          {recalls[activeRecallTab] && (
+            <div className="pt-3">
+              <h2 className="text-lg font-semibold">
+                Recall Details for {recalls[activeRecallTab].Component}
+              </h2>
+              <RecallInfoBox recallItem={recalls[activeRecallTab]} />
+            </div>
+          )}
+        </>
+      ) : (
+        <p>No recall information available.</p>
+      )}
+    </div>
+  );
+};
 
 export default VehicleRecalls;
