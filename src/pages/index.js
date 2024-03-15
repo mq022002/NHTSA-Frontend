3e2c68e42fc1c3865cef13d57c193989d48fba45
Matthew Quijano <mq022002@gmail.com>
2024-03-15 12:36:37 -0400

    feat: Add remaining routes as per project requirements
    - Standardized route naming convention. For example, instead of simply saying "Home", it would be "HomePage".

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BaseLayout from "../components/layouts/BaseLayout";
+
+function HomePage() {
+  return (
+    <BaseLayout>
+      <h1 className="text-black">HomePage</h1>
+    </BaseLayout>
+  );
+}
+
+export default HomePage;
