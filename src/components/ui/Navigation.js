03099c42611f27591506e7fa27213ed7d0103724
Matthew Quijano <mq022002@gmail.com>
2024-03-15 15:08:22 -0400

    refactor: Add link to FetchVehicleDataPage with conditional rendering

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,15 +1,33 @@
 import * as React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Navigation() {
+  const router = useRouter();
+
   return (
-    <nav className="flex items-center flex-wrap bg-[#dbd2c4] p-6  justify-between">
-      <img src="/TheHartfordIcon.png" alt="logo" className="px-2 h-12 w-auto" />
-      {/* Put a div tag here, and inside of the div tag, put a tags */}
-      <div>
-        <a href = "/">
-          The Home Page
-        </a>
-      </div>
+    <nav className="flex items-center bg-[#dbd2c4] p-6">
+      <img src="/TheHartfordIcon.png" alt="logo" className="w-auto h-12 px-2" />
+      <Link
+        href="/"
+        className={`mr-1 px-1 -mx-1 ${
+          router.pathname === "/"
+            ? "bg-[#832C31] p-1 rounded"
+            : "text-gray-700 font-normal"
+        }`}
+      >
+        Home
+      </Link>
+      <Link
+        href="/fetch_vehicle_data"
+        className={`mr-1 px-1 -mx-1 ${
+          router.pathname === "/fetch_vehicle_data"
+            ? "bg-[#832C31] p-1 rounded"
+            : "text-gray-700 font-normal"
+        }`}
+      >
+        Fetch Vehicle Data
+      </Link>
     </nav>
   );
 }
