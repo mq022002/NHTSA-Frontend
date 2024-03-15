49064dd85d9f47503fa676646380aed516357ec4
Matthew Quijano <mq022002@gmail.com>
2024-03-15 15:11:14 -0400

    refactor: Decomposed Navigation component to reduce redundancy

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -2,32 +2,28 @@ import * as React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-export default function Navigation() {
+const NavLink = ({ href, children }) => {
   const router = useRouter();
+  const isActive = router.pathname === href;
 
+  return (
+    <Link
+      href={href}
+      className={`mr-1 px-1 -mx-1 ${
+        isActive ? "bg-[#832C31] p-1 rounded" : "text-gray-700 font-normal"
+      }`}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default function Navigation() {
   return (
     <nav className="flex items-center bg-[#dbd2c4] p-6">
       <img src="/TheHartfordIcon.png" alt="logo" className="w-auto h-12 px-2" />
-      <Link
-        href="/"
-        className={`mr-1 px-1 -mx-1 ${
-          router.pathname === "/"
-            ? "bg-[#832C31] p-1 rounded"
-            : "text-gray-700 font-normal"
-        }`}
-      >
-        Home
-      </Link>
-      <Link
-        href="/fetch_vehicle_data"
-        className={`mr-1 px-1 -mx-1 ${
-          router.pathname === "/fetch_vehicle_data"
-            ? "bg-[#832C31] p-1 rounded"
-            : "text-gray-700 font-normal"
-        }`}
-      >
-        Fetch Vehicle Data
-      </Link>
+      <NavLink href="/">Home</NavLink>
+      <NavLink href="/fetch_vehicle_data">Fetch Vehicle Data</NavLink>
     </nav>
   );
 }
