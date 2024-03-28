327b0ef5c2c274ec885d30c7ce3c7ccc9f48456e
Matthew Quijano <mq022002@gmail.com>
2024-03-28 11:42:26 -0400

    feat: Restrict fetch_vehicle_data page to only authenticated users

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const NavLink = ({ href, children }) => {
   const router = useRouter();
@@ -21,6 +22,8 @@ const NavLink = ({ href, children }) => {
 };
 
 export default function Navigation() {
+  const { data: session } = useSession();
+
   return (
     <nav className="flex items-center justify-between bg-[#dbd2c4] p-2">
       <div className="flex items-center">
@@ -30,12 +33,25 @@ export default function Navigation() {
           className="w-auto h-12 pr-10"
         />
         <NavLink href="/">Home</NavLink>
-        <NavLink href="/fetch_vehicle_data">Fetch Vehicle Data</NavLink>
+        {session && (
+          <NavLink href="/fetch_vehicle_data">Fetch Vehicle Data</NavLink>
+        )}
       </div>
       <div className="flex items-center">
-        <NavLink href="/login">Login</NavLink>
-        <span className="pr-2 text-black  text-bold">/</span>
-        <NavLink href="/register">Register</NavLink> 
+        {session ? (
+          <>
+            <span className="pr-2 text-black text-bold">
+              {session.user.name}
+            </span>
+            <NavLink href="/api/auth/signout">Logout</NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink href="/login">Login</NavLink>
+            <span className="pr-2 text-black text-bold">/</span>
+            <NavLink href="/register">Register</NavLink>
+          </>
+        )}
       </div>
     </nav>
   );
