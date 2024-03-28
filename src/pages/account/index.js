f64401852a26f7da7686408890414f9bc02144bd
Matthew Quijano <mq022002@gmail.com>
2024-03-28 09:11:14 -0400

    refactor: Reduce redundancy by referencing BaseLayout globally

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import BaseLayout from "../../components/layouts/BaseLayout";
 
 function AccountPage() {
-  return (
-    <BaseLayout>
-      <h1 className="text-black">AccountPage</h1>
-    </BaseLayout>
-  );
+  return <h1 className="text-black">AccountPage</h1>;
 }
 
 export default AccountPage;
