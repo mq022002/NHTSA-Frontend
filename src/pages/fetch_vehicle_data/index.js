1eb0ae418ea189ec52d57cad8c44c693a7076a55
Matthew Quijano <mq022002@gmail.com>
2024-03-28 11:43:01 -0400

    feat: Prompt user to authenticate if they try to visit the link

--- src/pages/fetch_vehicle_data/index.js
+++ src/pages/fetch_vehicle_data/index.js
@@ -1,7 +1,25 @@
+import { getSession } from "next-auth/react";
 import VehicleFetcher from "../../components/vehicles/VehicleFetcher";
 
 function FetchVehicleDataPage() {
   return <VehicleFetcher />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default FetchVehicleDataPage;
