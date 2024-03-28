6322b543c5fd8cbce522e0b9f825c8fbc426080f
Matthew Quijano <mq022002@gmail.com>
2024-03-28 09:56:04 -0400

    feat: Add useSession() react hook to check if someone is signed in

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,26 @@
+// https://next-auth.js.org/getting-started/example
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
 function LoginPage() {
-  return <h1 className="text-black">LoginPage</h1>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <p>Signed in as {session.user.email}</p>
+        <button className="text-black" onClick={() => signOut()}>
+          Sign out
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <p className="text-black">Not signed in</p>
+      <button className="text-black" onClick={() => signIn()}>
+        Sign in
+      </button>
+    </>
+  );
 }
 export default LoginPage;
