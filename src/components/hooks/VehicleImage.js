5600dca0a44842a950c1d439eb8851077aea3089
Matthew Quijano <mq022002@gmail.com>
2024-03-29 01:12:43 -0400

    fix: ESLint errors for /components/hooks/VehicleImage

--- src/components/hooks/VehicleImage.js
+++ src/components/hooks/VehicleImage.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Image from "next/image";
 
 const VehicleImage = ({ scrapedData }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -6,21 +7,29 @@ const VehicleImage = ({ scrapedData }) => {
   return (
     scrapedData.imageUrl && (
       <div className="flex justify-center">
-      <div className="image-hover-wrapper " 
-           onMouseEnter={() => setIsHovered(true)} 
-           onMouseLeave={() => setIsHovered(false)}>
-        <a href={scrapedData.linkUrl} 
-           target="_blank" 
-           rel="noopener noreferrer">
-          <img src={scrapedData.imageUrl} 
-               alt="Car" 
-               className="max-w-[80%] max-h-[300px] object-cover border-2 border-red-500" />
-          <div className="image-hover-overlay" />
-          {isHovered && (
-            <div className="image-hover-text">Click to see cars for sale</div>
-          )}
-        </a>
-      </div>
+        <div
+          className="image-hover-wrapper"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <a
+            href={scrapedData.linkUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={scrapedData.imageUrl}
+              alt="Car"
+              className="max-w-[80%] max-h-[300px] object-cover border-2 border-red-500"
+              layout="fill"
+              objectFit="cover"
+            />
+            <div className="image-hover-overlay" />
+            {isHovered && (
+              <div className="image-hover-text">Click to see cars for sale</div>
+            )}
+          </a>
+        </div>
       </div>
     )
   );
