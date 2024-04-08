25d0b1c6d44c0fa43874584d3e2e50dc1f227355
Matthew Quijano <mq022002@gmail.com>
2024-04-08 11:29:18 -0400

    refactor: Move UserReviews to newly created UserReviewsPage

--- src/pages/user_reviews/index.js
+++ src/pages/user_reviews/index.js
@@ -1,5 +1,79 @@
+import UserReviews from "../../components/UserReviews";
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+
 function UserReviewsPage() {
-  return <h1 className="text-black">UserReviewsPage</h1>;
+  const { data: session } = useSession();
+
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // Fetch user reviews from the API
+    const fetchReviews = async () => {
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_FETCH_REVIEWS);
+        if (!response.ok) throw new Error("Failed to fetch");
+        const data = await response.json();
+        setReviews(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchReviews();
+  }, []);
+
+  const [newReview, setNewReview] = useState({
+    stars: 0,
+    reviewContent: "",
+  });
+
+  const handleRatingChange = (newRating) => {
+    setNewReview({ ...newReview, stars: newRating });
+  };
+
+  // Review submission logic
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!session) {
+      alert("You must be logged in to submit a review.");
+      return;
+    }
+
+    const reviewData = {
+      username: session.user.name,
+      reviewContent: newReview.reviewContent,
+      stars: newReview.stars,
+      datePosted: new Date().toISOString().slice(0, 10),
+    };
+
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_SUBMIT_REVIEW, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(reviewData),
+      });
+
+      if (!response.ok) throw new Error("Failed to submit review");
+      alert("Review submitted successfully");
+      e.target.reset();
+      setReviews([...reviews, reviewData]);
+    } catch (error) {
+      console.error("Failed to submit review:", error);
+      alert("Failed to submit review. Please try again.");
+    }
+    setNewReview({ stars: 0, reviewContent: "" });
+  };
+  return (
+    <div className="flex justify-center w-full">
+      <UserReviews
+        reviews={reviews}
+        newReview={newReview}
+        handleRatingChange={handleRatingChange}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 }
 
 export default UserReviewsPage;
