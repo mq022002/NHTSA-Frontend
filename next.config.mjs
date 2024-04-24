5b9639cc12142b0c5e08812ce81628037449b372
Matthew Quijano <mq022002@gmail.com>
2024-04-24 07:15:36 -0400

    feat: Make ./out if ENVIRONMENT is production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output:
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "production" ? "export" : "server",
+};
 
 export default nextConfig;
