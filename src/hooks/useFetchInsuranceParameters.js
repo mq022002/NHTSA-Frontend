6b393345bb677cf74fe1d4d6c29d3ace2a384a83
Matthew Quijano <mq022002@gmail.com>
2024-04-29 08:53:14 -0400

    refactor: Convert incoming insurance values to floats

--- src/hooks/useFetchInsuranceParameters.js
+++ src/hooks/useFetchInsuranceParameters.js
@@ -4,20 +4,27 @@ const useFetchInsuranceParameters = () => {
   const [parameters, setParameters] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const isProduction = process.env.NODE_ENV === "production";
 
   useEffect(() => {
     fetch(process.env.NEXT_PUBLIC_FETCH_INSURANCE_CALCULATIONS)
       .then((response) => response.json())
       .then((data) => {
         const { id, ...rest } = data;
-        setParameters(rest);
+        const parameters = Object.fromEntries(
+          Object.entries(rest).map(([key, value]) => [key, parseFloat(value)])
+        );
+        setParameters(parameters);
+        if (!isProduction) {
+          console.log(parameters);
+        }
         setIsLoading(false);
       })
       .catch((error) => {
         setError(error);
         setIsLoading(false);
       });
-  }, []);
+  }, [isProduction]);
 
   return { parameters, isLoading, error, setParameters };
 };
