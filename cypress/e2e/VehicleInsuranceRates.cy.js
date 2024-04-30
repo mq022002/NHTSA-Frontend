b408f82483c2cc3aeb3a0ab07d09da0d6d07fa63
Matthew Quijano <mq022002@gmail.com>
2024-04-30 04:49:30 -0400

    test: Add a wait for async functionality to complete

--- cypress/e2e/VehicleInsuranceRates.cy.js
+++ cypress/e2e/VehicleInsuranceRates.cy.js
@@ -27,6 +27,7 @@ describe("Vehicle Insurance Rates Flow", () => {
 
   it("should change the value of the year select field to 2017, click the button, and verify the text is visible", () => {
     cy.visit("http://localhost:3000/vehicle_insurance_rates");
+    cy.wait(500);
     cy.get('[data-testid="year-select"]')
       .should("not.be.disabled")
       .then(($select) => {
