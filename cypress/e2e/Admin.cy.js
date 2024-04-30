d6c8351663019c6dcce496c7e66c6d2b10036e27
Matthew Quijano <mq022002@gmail.com>
2024-04-30 07:04:06 -0400

    test: Test update insurance parameter functionality to Cypress tests

--- cypress/e2e/Admin.cy.js
+++ cypress/e2e/Admin.cy.js
@@ -1,7 +1,5 @@
 describe("Admin Functionality Flow", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/home");
-
     cy.window().then((win) => {
       win.localStorage.setItem("accessToken", "fake_access_token");
       win.localStorage.setItem("idToken", "fake_id_token");
@@ -19,12 +17,50 @@ describe("Admin Functionality Flow", () => {
         })
       );
     });
-
-    cy.visit("http://localhost:3000/home");
   });
 
   it("should navigate to admin page when Admin link is clicked", () => {
+    cy.visit("http://localhost:3000/home");
     cy.get("a").contains("Admin").click();
     cy.url().should("include", "/admin");
+  });
+
+  it("should change the value of the baseRate input and click the update parameters button", () => {
+    cy.visit("http://localhost:3000/admin");
+    cy.get('input[name="baseRate"]').clear().type("10000");
+    cy.contains("Update Parameters").click();
+  });
+
+  it("should select a vehicle, fetch data, and display vehicle details and insurance rate", () => {
+    cy.visit("http://localhost:3000/vehicle_insurance_rates");
+    cy.wait(2000);
+    cy.get('[data-testid="make-select"]').should("be.disabled");
+    cy.get('[data-testid="model-select"]').should("be.disabled");
+    cy.get('[data-testid="year-select"]')
+      .should("not.be.disabled")
+      .select("2017");
+    cy.get('[data-testid="make-select"]').should("not.be.disabled");
+    cy.get('[data-testid="fetch-data-button"]').click();
+    cy.contains("Please provide a year, make, and model.").should("be.visible");
+    cy.get('[data-testid="make-select"]')
+      .should("not.be.disabled")
+      .select("Honda");
+    cy.get('[data-testid="fetch-data-button"]').click();
+    cy.contains("Please provide a year, make, and model.").should("be.visible");
+    cy.get('[data-testid="model-select"]')
+      .should("not.be.disabled")
+      .select("Civic");
+    cy.get('[data-testid="fetch-data-button"]').click();
+    // cy.wait(2000);
+    // cy.contains("Please provide a year, make, and model.").should("not.exist");
+    // cy.contains("Vehicle: 2017 Honda Civic 2 DR FWD").should("be.visible");
+    // cy.contains("Vehicle: 2017 Honda Civic 2 DR FWD").click();
+    // cy.contains("Insurance Rate").should("be.visible");
+  });
+
+  it("should change the value of the baseRate input and click the update parameters button", () => {
+    cy.visit("http://localhost:3000/admin");
+    cy.get('input[name="baseRate"]').clear().type("500");
+    cy.contains("Update Parameters").click();
   });
 });
