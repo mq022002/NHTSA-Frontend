43bedac072cc6c5727e3ba8d3e497c526ff8566d
Matthew Quijano <mq022002@gmail.com>
2024-04-30 06:02:54 -0400

    test: Ensure that make field is disabled before and enabled after

--- cypress/e2e/VehicleInsuranceRates.cy.js
+++ cypress/e2e/VehicleInsuranceRates.cy.js
@@ -28,10 +28,12 @@ describe("Vehicle Insurance Rates Flow", () => {
   it("should change the value of the year select field to 2017, click the button, and verify the text is visible", () => {
     cy.visit("http://localhost:3000/vehicle_insurance_rates");
     cy.wait(500);
+    cy.get('[data-testid="make-select"]').should("be.disabled");
     cy.get('[data-testid="model-select"]').should("be.disabled");
     cy.get('[data-testid="year-select"]')
       .should("not.be.disabled")
       .select("2017");
+    cy.get('[data-testid="make-select"]').should("not.be.disabled");
     cy.get('[data-testid="fetch-data-button"]').click();
     cy.contains("Please provide a year, make, and model.").should("be.visible");
   });
