7613b4df4ed8f28c2663da96a34216b510909b85
Matthew Quijano <mq022002@gmail.com>
2024-04-30 05:14:25 -0400

    refactor: Ensure that objects are not rendered null

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -108,26 +108,27 @@ function AdminPage() {
       </div>
       <hr className="mb-4 border-b-2 border-gray-300" />
       <form onSubmit={handleSubmit} className="space-y-4">
-        {Object.entries(parameters).map(([key, currentVal]) => (
-          <div
-            key={key}
-            className="flex items-center justify-between space-x-4"
-          >
-            <label className="w-1/3 font-bold text-black">
-              {key.charAt(0).toUpperCase() +
-                key.slice(1).replace(/([A-Z])/g, " $1")}
-            </label>
-            <span className="w-1/3 text-center text-black">{currentVal}</span>
-            <input
-              type="text"
-              name={key}
-              placeholder="Enter new value"
-              value={editValues[key] || ""}
-              onChange={handleChange}
-              className="w-1/3 p-2 text-black border-2 border-gray-300 rounded-md"
-            />
-          </div>
-        ))}
+        {parameters &&
+          Object.entries(parameters).map(([key, currentVal]) => (
+            <div
+              key={key}
+              className="flex items-center justify-between space-x-4"
+            >
+              <label className="w-1/3 font-bold text-black">
+                {key.charAt(0).toUpperCase() +
+                  key.slice(1).replace(/([A-Z])/g, " $1")}
+              </label>
+              <span className="w-1/3 text-center text-black">{currentVal}</span>
+              <input
+                type="text"
+                name={key}
+                placeholder="Enter new value"
+                value={editValues[key] || ""}
+                onChange={handleChange}
+                className="w-1/3 p-2 text-black border-2 border-gray-300 rounded-md"
+              />
+            </div>
+          ))}
         <button
           type="submit"
           className="w-full px-4 py-2 mt-4 font-bold text-white bg-red-500 rounded hover:bg-red-700 md:w-auto"
