994539a5ec8deac1449e7894d7b667af9f46aeda
Matthew Quijano <mq022002@gmail.com>
2024-04-30 03:34:23 -0400

    test: Add superuser navigation flow

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -25,11 +25,11 @@ describe("Navigation and Authentication Flow", () => {
     cy.get("a").contains("Account").should("not.exist");
   });
 
-  it("should not display Admin link when user is not a superuser", () => {
+  it("should not display Admin link when user is not a superuser and when user is not logged in", () => {
     cy.get("a").contains("Admin").should("not.exist");
   });
 
-  describe("when logged in", () => {
+  describe("when logged in as regular user", () => {
     beforeEach(() => {
       cy.window().then((win) => {
         win.localStorage.setItem("accessToken", "fake_access_token");
@@ -67,6 +67,56 @@ describe("Navigation and Authentication Flow", () => {
 
     it("should not display Admin link when user is not a superuser", () => {
       cy.get("a").contains("Admin").should("not.exist");
+    });
+
+    it("should log out when Logout link is clicked", () => {
+      cy.get("a").contains("Logout").click();
+      cy.window().its("localStorage").should("be.empty");
+    });
+  });
+
+  describe("when logged in as superuser", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/home");
+
+      cy.window().then((win) => {
+        win.localStorage.setItem("accessToken", "fake_access_token");
+        win.localStorage.setItem("idToken", "fake_id_token");
+        win.localStorage.setItem("refreshToken", "fake_refresh_token");
+        win.localStorage.setItem(
+          "cognitoUser",
+          JSON.stringify({
+            sub: "fake-sub",
+            aud: "fake-aud",
+            email: "fake-email@example.com",
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            iat: Math.floor(Date.now() / 1000),
+            name: "fake-name",
+            "cognito:groups": ["superusers"],
+          })
+        );
+      });
+
+      cy.visit("http://localhost:3000/home");
+    });
+
+    it("should display the user's name", () => {
+      cy.get("a").contains("fake-name").should("be.visible");
+    });
+
+    it("should navigate to account page when user's name is clicked", () => {
+      cy.get("a").contains("fake-name").click();
+      cy.url().should("include", "/account");
+    });
+
+    it("should navigate to vehicle insurance rates page when Vehicle Insurance Rates link is clicked", () => {
+      cy.get("a").contains("Vehicle Insurance Rates").click();
+      cy.url().should("include", "/vehicle_insurance_rates");
+    });
+
+    it("should navigate to admin page when Admin link is clicked", () => {
+      cy.get("a").contains("Admin").click();
+      cy.url().should("include", "/admin");
     });
 
     it("should log out when Logout link is clicked", () => {
